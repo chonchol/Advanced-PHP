eeb5e638b19ded80ac83708416c369d1e02940ba
chonchol <chonchol57@gmail.com>
2015-10-15 23:31:10 +0600

    Create shapes02.php

---
+++ shapes02.php
@@ -0,0 +1,17 @@
+<?php
+$num=1;
+$line=5;
+	for($i=1;$i<=$line;$i++){
+		
+		for($j=1;$j<=$line-$i;$j++){
+			echo "&nbsp&nbsp";
+		}
+		
+		for($j=1;$j<=$num;$j++){
+			echo "*";
+		}
+		echo "<br>";
+		
+		$num=$num+1;
+	}
+?>
